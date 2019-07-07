6bd845a64ada06ef4f9b675760592f226d8e9037
287f7556c4925cb8127d7fc1be9ab711 <turkhackteam.istihbarattim.osint@gmail.com>
2019-07-07 15:26:41 -0400

    hunter

--- hunter.py
+++ hunter.py
@@ -3,9 +3,6 @@
 import os
 from pyhunter import PyHunter
 
-api = "44ac9d0fcf060465933c1591d75c2ace4b1692d8"
-hunter = PyHunter(api)
-
 
 def domain_search(domain):
     result = hunter.domain_search(domain)
@@ -41,36 +38,63 @@ def email_verifier(email):
 
 
 try:
-    os.system("clear")
-    os.system("figlet TurkHackTeam\n")
-    print("\033[94mİstihbarat Tim Osint Hunter Aracına Hoş Geldiniz")
-    print("""
-    1 - Domain Arama
-    2 - Email Bulma
-    3 - Email Doğrulama
-    0 - Çıkış
-    """)
-    choise = input("Yapmak İstediğiniz İşlemi Seçiniz = ")
+    while (True):
+        api = "44ac9d0fcf060465933c1591d75c2ace4b1692d8"
+        hunter = PyHunter(api)
+        info = (hunter.account_information())
+        os.system("clear")
+        os.system("figlet TurkHackTeam\n")
+        print("\033[94mİstihbarat Tim Osint Hunter Aracına Hoş Geldiniz")
 
-    if (choise == "1"):
-        domain = input("\nHedef Domain Giriniz (turkhackteam.org) = ")
-        domain_search(domain)
+        if (info["calls"]["left"] != 0):
+            print("Mevcut Api İçin Kalan Arama Hakkı  = " + info["calls"]["left"])
+            print("""
+            1 - Domain Arama
+            2 - Email Bulma
+            3 - Email Doğrulama
+            0 - Çıkış
+            """)
+            choise = input("Yapmak İstediğiniz İşlemi Seçiniz = ")
 
-    elif (choise == "2"):
-        domain = input("\nİlgili Domain/Şirket Giriniz (org) = ")
-        ad = input("İlgili Kullanıcı Adı Giriniz (emre) = ")
-        soyad = input("İlgili Kullanıcı Soyadı Giriniz (öztürk) = ")
-        email_finder(domain, ad, soyad)
+            if (choise == "1"):
+                domain = input("\nHedef Domain Giriniz (turkhackteam.org) = ")
+                domain_search(domain)
 
-    elif (choise == "3"):
-        email = input("\nİlgili Email Adresini Giriniz (root@turkhackteam.org) = ")
-        email_verifier(email)
+            elif (choise == "2"):
+                domain = input("\nİlgili Domain/Şirket Giriniz (org) = ")
+                ad = input("İlgili Kullanıcı Adı Giriniz (emre) = ")
+                soyad = input("İlgili Kullanıcı Soyadı Giriniz (öztürk) = ")
+                email_finder(domain, ad, soyad)
 
-    elif (choise == "0"):
-        print("\n\033[91mNE MUTLU TÜRKÜM DİYENE")
+            elif (choise == "3"):
+                email = input("\nİlgili Email Adresini Giriniz (root@turkhackteam.org) = ")
+                email_verifier(email)
 
-    else:
-        print("Hatalı Bir Seçim Yaptınız, Tekrar Deneyiniz..")
+            elif (choise == "0"):
+                print("\n\033[91mNE MUTLU TÜRKÜM DİYENE")
+                break
+
+            else:
+                print("Hatalı Bir Seçim Yaptınız, Tekrar Deneyiniz..")
+
+        else:
+            print("\nMevcut Api Arama Hakkı Bitti")
+            print("""
+                1 - Yeni Api Eklemek
+                0 - Çıkış Yapamk
+                """)
+
+            choise = input("Yapmak İstediğiniz İşlemi Seçiniz = ")
+
+            if (choise == "1"):
+                api = input("\nYeni Api Değerini Giriniz = ")
+            elif (choise == "0"):
+                print("\n\033[91mNE MUTLU TÜRKÜM DİYENE")
+                break
+            else:
+                print("Hatalı Bir Seçim Yaptınız, Tekrar Deneyiniz..")
+
+
 except(KeyboardInterrupt):
     print("\nÇıkış Yaptınız..")
 except:
