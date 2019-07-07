8fec8557adacebd4e45e13cdb5fe3a4d76f64c2f
287f7556c4925cb8127d7fc1be9ab711 <turkhackteam.istihbarattim.osint@gmail.com>
2019-07-07 09:45:39 -0400

    hunter

--- hunter.py
+++ hunter.py
@@ -1,15 +1,8 @@
 import os
-import optparse
 from pyhunter import PyHunter
 
 api = "44ac9d0fcf060465933c1591d75c2ace4b1692d8"
-
-"""
-def get_user_input():
-    parse = optparse.OptionParser()
-    parse.add_option("-d", "--domain", dest="domain", help="hedef domain adres")
-    return parse.parse_args()
-"""
+hunter = PyHunter(api)
 
 
 def domain_search(domain):
@@ -20,16 +13,27 @@ def domain_search(domain):
 
     for i in range(len(result)):
         for j in range(len(keys)):
-            os.system("echo " + keys[j] + " = " + str(result[i][values[j]]) + " >> deneme.txt")
-        os.system("echo " + "-----------------------------------------" + " >> deneme.txt")
+            os.system("echo " + keys[j] + " = " + str(result[i][values[j]]) + " >> " + domain + ".txt")
+        os.system("echo " + "-----------------------------------------" + " >> " + domain + ".txt")
+    print("\nSonuçlar " + domain + ".txt Olarak Kayıt Edildi..")
 
 
-# def email_finder():
+def email_finder(ad, soyad, domain):
+    ad_soyad = ad + " " + soyad
+    result = hunter.email_finder(company=domain, full_name=ad_soyad)
+    result2 = hunter.email_finder(company=domain, full_name=ad_soyad, raw=True)
+    print(result[0])
+    print(result2)
 
 
-# def email_verifier():
+def email_verifier(email):
+    list = ["mx_records", "smtp_server", "smtp_check"]
+    result = hunter.email_verifier(email)
+    for i in list:
+        os.system("echo " + i + " = " + str(result[i]) + " >> " + email + ".txt")
+    print("\nSonuçlar " + email + ".txt Olarak Kayıt Edildi..")
 
-hunter = PyHunter(api)
+
 os.system("figlet TurkHackTeam\n")
 print("İstihbarat Tim Osint Hunter Aracına Hoş Geldiniz")
 print("""
@@ -44,9 +48,15 @@ if (choise == "1"):
     domain = input("\nHedef Domain Giriniz = ")
     domain_search(domain)
 
-# elif (choise == "2"):
+elif (choise == "2"):
+    domain = input("İlgili Domain/Şirket Giriniz = ")
+    ad = input("İlgili Kullanıcı Adı Giriniz = ")
+    soyad = input("İlgili Kullanıcı Soyadı Giriniz = ")
+    email_finder(domain, ad, soyad)
 
-# elif (choise == "3"):
+elif (choise == "3"):
+    email = input("İlgili Email Adresini Giriniz = ")
+    email_verifier(email)
 
 elif (choise == "0"):
     print("\nNE MUTLU TÜRKÜM DİYENE")
