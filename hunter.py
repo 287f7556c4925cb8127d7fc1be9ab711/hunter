2d6eaaefa3c0f1d1fc4c6baf363958c8f5384f50
root <root@localhost.localdomain>
2019-07-07 08:05:33 -0400

    hunter

--- hunter.py
+++ hunter.py
@@ -1,17 +1,54 @@
+import os
+import optparse
 from pyhunter import PyHunter
 
 api = "44ac9d0fcf060465933c1591d75c2ace4b1692d8"
 
+"""
+def get_user_input():
+    parse = optparse.OptionParser()
+    parse.add_option("-d", "--domain", dest="domain", help="hedef domain adres")
+    return parse.parse_args()
+"""
+
+
+def domain_search(domain):
+    result = hunter.domain_search(domain)
+    result = result["emails"]
+    for i in range(len(result)):
+        os.system("echo " + "ad = " + str(result[i]["first_name"]) + " >> deneme.txt")
+        os.system("echo " + "soyad = " + str(result[i]["last_name"]) + " >> deneme.txt")
+        os.system("echo " + "telefon = " + str(result[i]["phone_number"]) + " >> deneme.txt")
+        os.system("echo " + "email = " + str(result[i]["value"]) + " >> deneme.txt")
+        os.system("echo " + "-----------------------------------------" + " >> deneme.txt")
+
+
+#def email_finder():
+
+
+# def email_verifier():
+
 hunter = PyHunter(api)
+os.system("figlet TurkHackTeam\n")
+print("İstihbarat Tim Osint Hunter Aracına Hoş Geldiniz")
+print("""
+1 - Domain Arama
+2 - Email Bulma
+3 - Email Doğrulama
+0 - Çıkış
+""")
+choise = input("Yapmak İstediğiniz İşlemi Seçiniz = ")
 
-result = hunter.domain_search("ankara.edu.tr")
-result = result["emails"]
+if (choise == "1"):
+    domain = input("\nHedef Domain Giriniz = ")
+    domain_search(domain)
 
-for i in range(len(result)):
-    print("-----------------------------------------")
-    print("|  ad = ", result[i]["first_name"])
-    print("|  soyad = ", result[i]["last_name"])
-    print("|  telefon = ", result[i]["phone_number"])
-    print("|  email = ", result[i]["value"])
-    print("-----------------------------------------")
-    print("\n")
+# elif (choise == "2"):
+
+# elif (choise == "3"):
+
+elif (choise == "0"):
+    print("\nNE MUTLU TÜRKÜM DİYENE")
+
+else:
+    print("Hatalı Bir Seçim Yaptınız, Tekrar Deneyiniz..")
