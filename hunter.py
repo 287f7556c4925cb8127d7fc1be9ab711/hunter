25a20d0d981ae3ac4eade2417d39bac5212c15ae
287f7556c4925cb8127d7fc1be9ab711 <turkhackteam.istihbarattim.osint@gmail.com>
2019-07-07 12:30:59 -0400

    hunter

--- hunter.py
+++ hunter.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 from pyhunter import PyHunter
 
@@ -10,10 +12,16 @@ def domain_search(domain):
     result = result["emails"]
     values = ["first_name", "last_name", "phone_number", "value"]
     keys = ["ad", "soyad", "telefon", "email"]
-
     for i in range(len(result)):
+        email = hunter.email_verifier(result[i]["value"])
         for j in range(len(keys)):
             os.system("echo " + keys[j] + " = " + str(result[i][values[j]]) + " >> " + domain + ".txt")
+
+        if (email["smtp_check"]):
+            os.system("echo " + keys[3] + " durumu = email aktif durumda" + " >> " + domain + ".txt")
+        else:
+            os.system("echo " + keys[3] + " durumu = email aktif değildir" + " >> " + domain + ".txt")
+
         os.system("echo " + "-----------------------------------------" + " >> " + domain + ".txt")
     print("\nSonuçlar " + domain + ".txt Olarak Kayıt Edildi..")
 
@@ -21,45 +29,51 @@ def domain_search(domain):
 def email_finder(ad, soyad, domain):
     ad_soyad = ad + " " + soyad
     result = hunter.email_finder(company=domain, full_name=ad_soyad)
-    result2 = hunter.email_finder(company=domain, full_name=ad_soyad, raw=True)
-    print(result[0])
-    print(result2)
+    print("\n" + result[0])
 
 
 def email_verifier(email):
-    list = ["mx_records", "smtp_server", "smtp_check"]
     result = hunter.email_verifier(email)
-    for i in list:
-        os.system("echo " + i + " = " + str(result[i]) + " >> " + email + ".txt")
-    print("\nSonuçlar " + email + ".txt Olarak Kayıt Edildi..")
+    if (result["smtp_check"]):
+        print("\nsmtp_check = " + str(result["smtp_check"]) + "\t--->\tBöyle Bir Email Adresi Vardır")
+    else:
+        print("\nsmtp_check = " + str(result["smtp_check"]) + "\t--->\tBöyle Bir Email Adresi Yoktur")
 
 
-os.system("figlet TurkHackTeam\n")
-print("İstihbarat Tim Osint Hunter Aracına Hoş Geldiniz")
-print("""
-1 - Domain Arama
-2 - Email Bulma
-3 - Email Doğrulama
-0 - Çıkış
-""")
-choise = input("Yapmak İstediğiniz İşlemi Seçiniz = ")
+try:
+    os.system("clear")
+    os.system("figlet TurkHackTeam\n")
+    print("\033[94mİstihbarat Tim Osint Hunter Aracına Hoş Geldiniz")
+    print("""
+    1 - Domain Arama
+    2 - Email Bulma
+    3 - Email Doğrulama
+    0 - Çıkış
+    """)
+    choise = input("Yapmak İstediğiniz İşlemi Seçiniz = ")
 
-if (choise == "1"):
-    domain = input("\nHedef Domain Giriniz = ")
-    domain_search(domain)
+    if (choise == "1"):
+        domain = input("\nHedef Domain Giriniz = ")
+        domain_search(domain)
 
-elif (choise == "2"):
-    domain = input("İlgili Domain/Şirket Giriniz = ")
-    ad = input("İlgili Kullanıcı Adı Giriniz = ")
-    soyad = input("İlgili Kullanıcı Soyadı Giriniz = ")
-    email_finder(domain, ad, soyad)
+    elif (choise == "2"):
+        domain = input("\nİlgili Domain/Şirket Giriniz = ")
+        ad = input("İlgili Kullanıcı Adı Giriniz = ")
+        soyad = input("İlgili Kullanıcı Soyadı Giriniz = ")
+        email_finder(domain, ad, soyad)
 
-elif (choise == "3"):
-    email = input("İlgili Email Adresini Giriniz = ")
-    email_verifier(email)
+    elif (choise == "3"):
+        email = input("\nİlgili Email Adresini Giriniz = ")
+        email_verifier(email)
 
-elif (choise == "0"):
-    print("\nNE MUTLU TÜRKÜM DİYENE")
+    elif (choise == "0"):
+        print("\n\033[91mNE MUTLU TÜRKÜM DİYENE")
 
-else:
-    print("Hatalı Bir Seçim Yaptınız, Tekrar Deneyiniz..")
+    else:
+        print("Hatalı Bir Seçim Yaptınız, Tekrar Deneyiniz..")
+except(KeyboardInterrupt):
+    print("\nÇıkış Yaptınız..")
+except:
+    print("\nBir Hata Oluştu..")
+finally:
+    print("\nposew7@turkhackteam.org")
---
+++ setup.sh
@@ -0,0 +1,5 @@
+apt-get install python3-pip
+apt-get install figlet
+pip3 install pyhunter
+
+echo "kurulum başarıyla tamamlandı" 
